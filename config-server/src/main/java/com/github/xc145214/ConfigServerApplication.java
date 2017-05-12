d215623f6cb21cfe7c86e7200c0be7fd336e8fcc
xc145214 <xc145214@gmail.com>
2017-05-12 17:39:50 +0800

    配置中为本地化配置

---
+++ config-server/src/main/java/com/github/xc145214/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.github.xc145214;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+    }
+}
