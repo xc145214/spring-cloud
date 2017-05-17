9afa3e311ed290b4e781195bdbeea747dfbd5ff4
xc145214 <xc145214@gmail.com>
2017-05-17 17:30:19 +0800

    config-client 从config-server服务端获取配置

---
+++ config-client/src/main/java/com/github/xc145214/ConfigClientApplication.java
@@ -0,0 +1,28 @@
+/*************************************************************************
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *
+ *                COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ *    ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *************************************************************************/
+package com.github.xc145214;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author xiachuan at 2017/5/17 17:13。
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+    }
+}
+
---
+++ config-client/src/main/java/com/github/xc145214/web/TestController.java
@@ -0,0 +1,37 @@
+/*************************************************************************
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *
+ *                COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ *    ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *************************************************************************/
+package com.github.xc145214.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author xiachuan at 2017/5/17 17:14。
+ */
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
+}
+
