8ab72587e9af984f533a4c0d15ad2b733e03455a
xc145214 <xc145214@gmail.com>
2017-05-17 18:18:36 +0800

    服务网关

---
+++ service-A/src/main/java/com/github/xc145214/web/AServiceController.java
@@ -0,0 +1,43 @@
+/*************************************************************************
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *
+ *                COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ *    ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *************************************************************************/
+package com.github.xc145214.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author xiachuan at 2017/5/17 18:02。
+ */
+@RestController
+public class AServiceController {
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public String add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return "From Service-A, Result is " + r;
+    }
+}
+
